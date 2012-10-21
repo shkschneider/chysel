08b86adce7e54a47fd7df0b1e3d1816ddac1740a
shkschneider <shk.schneider@gmail.com>
2012-10-21 15:12:10 +0200

    Reorganized code from chizel by sz

--- chisel.py
+++ chisel.py
@@ -1,39 +1,47 @@
-#!/usr/bin/python
+#!/usr/bin/env python
+#
+# chisel
+#   David Zhou <https://github.com/dz/chisel>
+#   Alan Schneider <https://github.com/shkschneider/chisel>
+#
 
-# Chisel
-# David Zhou
-# 
-# Requires:
-# jinja2
+### DEPENDS: jinja2 markdown ###
 
-import sys, re, time, os, codecs
-import jinja2, markdown
+try:
+    from distutils import dir_util
+    import markdown
+    import codecs
+    import jinja2
+    import time
+    import sys
+    import os
+    import re
+except ImportError as error:
+    print 'ImportError: ', str(error)
+    exit(1)
 
-#Settings
-SOURCE = "./blog/" #end with slash
-DESTINATION = "./export/" #end with slash
-HOME_SHOW = 15 #numer of entries to show on homepage
-TEMPLATE_PATH = "./templates/"
+### SETTINGS ###
+
+INPUT = './content/' # ends with slash
+OUTPUT = './www/' # ends with slash
+HOME_SHOW = 15
+TEMPLATE_PATH = './templates/'
 TEMPLATE_OPTIONS = {}
-TEMPLATES = {
-    'home': "home.html",
-    'detail': "detail.html",
-    'archive': "archive.html",
-}
-TIME_FORMAT = "%B %d, %Y"
-ENTRY_TIME_FORMAT = "%m/%d/%Y"
-#FORMAT should be a callable that takes in text
-#and returns formatted text
-FORMAT = lambda text: markdown.markdown(text, ['footnotes',]) 
-#########
+TEMPLATES = {'index': 'index.html',
+             'page': 'page.html',
+             'archive': 'archive.html'}
+TIME_FORMAT = '%B %d, %Y'
+ENTRY_TIME_FORMAT = '%m/%d/%Y'
+FORMAT = lambda text: markdown.markdown(text, ['footnotes'])
+
+### DO NOT EDIT BELOW THIS LINE ###
 
 STEPS = []
 
 def step(func):
     def wrapper(*args, **kwargs):
-        print "Starting " + func.__name__ + "...",
+        print func.__name__
         func(*args, **kwargs)
-        print "Done."
     STEPS.append(wrapper)
     return wrapper
 
@@ -41,25 +49,23 @@ def get_tree(source):
     files = []
     for root, ds, fs in os.walk(source):
         for name in fs:
-            if name[0] == ".": continue
+            if name[0] == '.':
+                continue
             path = os.path.join(root, name)
-            f = open(path, "rU")
-            title = f.readline()
-            date = time.strptime(f.readline().strip(), ENTRY_TIME_FORMAT)
-            year, month, day = date[:3]
-            files.append({
-                'title': title,
-                'epoch': time.mktime(date),
-                'content': FORMAT(''.join(f.readlines()[1:]).decode('UTF-8')),
-                'url': '/'.join([str(year), "%.2d" % month, "%.2d" % day, os.path.splitext(name)[0] + ".html"]),
-                'pretty_date': time.strftime(TIME_FORMAT, date),
-                'date': date,
-                'year': year,
-                'month': month,
-                'day': day,
-                'filename': name,
-            })
-            f.close()
+            with open(path, 'rU') as f:
+                title = f.readline()
+                date = time.strptime(f.readline().strip(), ENTRY_TIME_FORMAT)
+                year, month, day = date[:3]
+                files.append({'title': title,
+                              'epoch': time.mktime(date),
+                              'content': FORMAT(''.join(f.readlines()[1:]).decode('UTF-8')),
+                              'url': '/'.join([str(year), '%.2d' % month, '%.2d' % day, os.path.splitext(name)[0] + '.html']),
+                              'pretty_date': time.strftime(TIME_FORMAT, date),
+                              'date': date,
+                              'year': year,
+                              'month': month,
+                              'day': day,
+                              'filename': name})
     return files
 
 def compare_entries(x, y):
@@ -69,45 +75,49 @@ def compare_entries(x, y):
     return result
 
 def write_file(url, data):
-    path = DESTINATION + url
+    path = OUTPUT + url
     dirs = os.path.dirname(path)
     if not os.path.isdir(dirs):
         os.makedirs(dirs)
-    file = open(path, "w")
-    file.write(data.encode('UTF-8'))
-    file.close()
+    with open(path, 'w') as f:
+        f.write(data.encode('UTF-8'))
 
 @step
-def generate_homepage(f, e):
-    """Generate homepage"""
-    template = e.get_template(TEMPLATES['home'])
-    write_file("index.html", template.render(entries=f[:HOME_SHOW]))
+def step_index(f, e):
+    '''Generate homepage'''
+    template = e.get_template(TEMPLATES['index'])
+    write_file('index.html', template.render(entries=f[:HOME_SHOW]))
 
 @step
-def master_archive(f, e):
-    """Generate master archive list of all entries"""
-    template = e.get_template(TEMPLATES['archive'])
-    write_file("archives.html", template.render(entries=f))
-
-@step
-def detail_pages(f, e):
-    """Generate detail pages of individual posts"""
-    template = e.get_template(TEMPLATES['detail'])
+def step_page(f, e):
+    '''Generate detail pages of individual posts'''
+    template = e.get_template(TEMPLATES['page'])
     for file in f:
         write_file(file['url'], template.render(entry=file))
 
-def main():
-    print "Chiseling..."
-    print "\tReading files...",
-    files = sorted(get_tree(SOURCE), cmp=compare_entries)
-    env = jinja2.Environment(loader=jinja2.FileSystemLoader(TEMPLATE_PATH), **TEMPLATE_OPTIONS)
-    print "Done."
-    print "\tRunning steps..."
-    for step in STEPS:
-        print "\t\t",
-        step(files, env)
-    print "\tDone."
-    print "Done."
+@step
+def step_archive(f, e):
+    '''Generate master archive list of all entries'''
+    template = e.get_template(TEMPLATES['archive'])
+    write_file('archive.html', template.render(entries=f))
 
-if __name__ == "__main__":
+@step
+def step_assets(f, e):
+    '''Copies assets'''
+    for name in [name for name in os.listdir(TEMPLATE_PATH) if os.path.isdir(os.path.join(TEMPLATE_PATH, name))]:
+        dir_util.copy_tree(TEMPLATE_PATH + name, OUTPUT + name)
+
+def main():
+    print 'Chiseling...'
+    print '* Reading files...',
+    files = sorted(get_tree(INPUT), cmp=compare_entries)
+    env = jinja2.Environment(loader=jinja2.FileSystemLoader(TEMPLATE_PATH), **TEMPLATE_OPTIONS)
+    print 'Done.'
+    print '* Running steps...'
+    for step in STEPS:
+        print '  ',
+        step(files, env)
+    print 'Done.'
+
+if __name__ == '__main__':
     sys.exit(main())
